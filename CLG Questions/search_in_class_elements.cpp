1783f8d86549cd1eefccdf7c9c72797d2e3c0d84
Abhishek Singh <abhishek120106@gmail.com>
2024-10-01 17:15:39 +0530

    Create search_in_class_elements.cpp

---
+++ CLG Questions/search_in_class_elements.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+class data{
+    int a;
+    public:
+    void scan(){
+        cin>>a;
+    }
+    void print(){
+        cout<<a<<' ';
+    }
+    int search(int ele){
+        return (a==ele?1:0);
+    }
+};
+
+
+int main(){
+    data d1[6];
+    for (int i = 0; i < 6; i++)
+    {
+        d1[i].scan();
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        d1[i].print();
+    }
+    int ele;
+    cout<<"\nenter element to search ";
+    cin>>ele;
+    for (int i = 0; i < 6; i++)
+    {
+        int flag=0;
+        flag=d1[i].search(ele);
+
+        if(flag==1) {
+            cout<<"\nelement found in class "<<i+1;
+            break;
+        }
+    }
+    return 0;
+}
