9e22c7592f2dac4c0eec3c78aabe397cd315668b
Abhishek Singh <abhishek120106@gmail.com>
2024-11-21 17:20:19 +0530

    Create greater_in_object.cpp

---
+++ CLG Questions/greater_in_object.cpp
@@ -0,0 +1,33 @@
+// 23/09/2024
+#include<iostream>
+using namespace std;
+
+class Greater {
+
+    int a,b;
+    public:
+
+    void s(){
+        cin>>a>>b;
+    }
+
+    void p(){
+        cout<<a<<" "<<b;
+    }
+
+    void cla(Greater p,Greater p2){
+        a= p.a>p2.a ? p.a: p2.a;
+        b= p.b>p2.b ? p.b: p2.b;
+    }
+};
+
+int main(){
+    Greater gc,gc2,gc3;
+
+    gc.s();
+    gc2.s();
+
+    gc3.cla(gc,gc2);
+    gc3.p();
+    return 0;
+}
